513ebf0b2979c5a137e067d20598aebef99d3545
Keerthikumarreddy-kanchamreddy <122507926+Keerthikumarreddy-kanchamreddy@users.noreply.github.com>
2024-07-03 17:31:40 -0400

    code improvements

--- Postaway/src/features/post/routes/post.routes.js
+++ Postaway/src/features/post/routes/post.routes.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { getAllPosts,getPostById,createPost,deletePost,updatePost } from '../controller/post.controller.js';
+import { getAllPosts,getPostById,getPostsByUserId,createPost,deletePost,updatePost } from '../controller/post.controller.js';
 
 const router = express.Router();
 
 router.route('/all').get(getAllPosts);
 router.route('/:id').get(getPostById);
+router.route('/').get(getPostsByUserId);
 router.route('/').post(createPost);
 router.route('/:id').delete(deletePost);
 router.route('/:id').put(updatePost);
